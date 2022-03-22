f66683cbe2beb4862067fcacc4ce5dc0c378dc99
Collins Oriakhi <collins.oriakhi@itara.co>
2022-03-22 17:14:45 +0100

    Initial Commit

---
+++ WeatherAppUI/WeatherAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppUIApp.swift
+//  WeatherAppUI
+//
+//  Created by Oriakhi Collins on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
